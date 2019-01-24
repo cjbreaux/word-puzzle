941fd4bdfe9455f2f9b1796c597091062386d7c0
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-23 21:18:20 -0800

    replace all vowels with dashes and will add another button to display the answer

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,25 @@ $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
     var outputText = $("input#inputSentence").val();
-    $("#outputSentence").text(outputText);
+    var output = ""
+
+    for(i=0 ;i<outputText.length; i+=1) {
+      if(outputText[i]==="a" || outputText[i]==="e" || outputText[i]==="i" || outputText[i]==="o" || outputText[i]==="u"){
+      	output+="-";
+      } else {
+      	output+=outputText[i];
+     	 }
+    }
+
+    $("#outputSentence").text(output);
     $(".outputSentence").show();
+    $("form").hide();
+
+    $("button#answerBtn").click(function(){
+      $("#answer").text(outputText);
+    })
+
   })
+
+
 });
